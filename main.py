47ff399510f50f4fd1bfb47af1ebb68d295dccdc
Thorgal <71973546+Thorgal108@users.noreply.github.com>
2021-05-21 16:49:16 +0200

    Added presenc

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from discord_slash import SlashCommand
 default_intents = discord.Intents.default()
 intents = discord.Intents(messages=True)
 
-
 class GigaChad(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="gc!", intents=intents)
@@ -19,11 +18,13 @@ class GigaChad(commands.Bot):
         print(self.user.id)
         print(discord.__version__)
         print('------')
+        await gigachad.change_presence(activity=discord.Game(name="Type gc!help for help"))
 
 
 gigachad = GigaChad()
 
 gigachad.remove_command('help')
+
 
 slash = SlashCommand(gigachad, sync_commands=True, sync_on_cog_reload=True)
 
