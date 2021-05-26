6ccf38e6760e9dee0138794e2ffd60862ed12dd4
Thorgal <71973546+Thorgal108@users.noreply.github.com>
2021-05-26 10:02:48 +0200

    Added the prefix command + rework of get_prefix

--- cogs/prefix.py
+++ cogs/prefix.py
@@ -4,56 +4,56 @@ import json
 from discord.ext import commands
 
 
-def get_prefix(bot, message):
-    try:
+def get_prefix(bot, message, raw=False):
+    if message.guild is not None:
         with open('prefixes.json', 'r') as f:
             prefixes = json.load(f)
-            return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot, message)
 
-    except KeyError:
-        with open('prefixes.json', 'r') as k:
-            prefixes = json.load(k)
-        prefixes[str(message.guild.id)] = 'gc!'
+        if str(message.guild.id) in prefixes:
+            if raw:
+                return prefixes[str(message.guild.id)]
+            else:
+                return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot, message)
 
-        with open('prefixes.json', 'w') as j:
-            json.dump(prefixes, j, indent=4)
+        else:
+            if raw:
+                return "gc!"
+            else:
+                return commands.when_mentioned_or("gc!")(bot, message)
 
-        with open('prefixes.json', 'r') as t:
-            prefixes = json.load(t)
-            return commands.when_mentioned_or(prefixes[str(message.guild.id)])(bot, message)
-
-    except:
-        return commands.when_mentioned_or("gc!")(bot, message)
-
-
-def get_prefix_raw(message):
-    try:
-        with open('prefixes.json', 'r') as f:
-            prefixes = json.load(f)
-            return prefixes[str(message.guild.id)]
-            print('was found')
-
-    except KeyError:
-        print('was not found')
-        with open('prefixes.json', 'r') as k:
-            prefixes = json.load(k)
-        prefixes[str(message.guild.id)] = 'gc!'
-
-        with open('prefixes.json', 'w') as j:
-            json.dump(prefixes, j, indent=4)
-
-        with open('prefixes.json', 'r') as t:
-            prefixes = json.load(t)
-            return prefixes[str(message.guild.id)]
-
-    except:
-        print('was in dm')
-        return "gc!"
+    else:
+        if raw:
+            return "gc!"
+        else:
+            return commands.when_mentioned_or("gc!")(bot, message)
 
 
 class Prefix(commands.Cog):
     def __init__(self, gigachad):
         self.gigachad = gigachad
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def prefix(self, ctx, prefix=None):
+        if prefix is None:
+            current_prefix = get_prefix(self, ctx, True)
+            embed = discord.Embed(color=0x2f3136, title="Current Prefix",
+                                  description=f"My current prefix is `{current_prefix}`. \n Execute `{current_prefix}pre"
+                                              "fix newprefix` to set a new prefix!")
+
+        else:
+            with open('prefixes.json', 'r') as f:
+                prefixes = json.load(f)
+
+            prefixes[str(ctx.guild.id)] = prefix
+
+            with open('prefixes.json', 'w') as f:
+                json.dump(prefixes, f, indent=4)
+
+            embed = discord.Embed(color=0x57f287, title="Prefix succesfully changed",
+                                  description=f"My prefix for this server has succefully been changed to `{prefix}`.")
+
+        await ctx.send(embed=embed)
 
 
 def setup(gigachad):
