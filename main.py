b827c848e746c5903f9deb9cc4e6922aeda62dc2
Orh <71973546+O4h@users.noreply.github.com>
2022-03-01 07:44:26 +0100

    Switch to Disnake (#14)
    A lot of other stuff:
    - remove text commands
    - removed the gallery command
    - removed the prefix database

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
 import asyncio
 import os
-
 import asyncpg
-import discord
-import jishaku
+import disnake
 import sentry_sdk
-from cogs.prefix import get_prefix
-from discord.ext import commands
-from discord_slash import SlashCommand
-from util.help import CustomHelp
 
-intents = discord.Intents(messages=True, guilds=True)
+
+from disnake.ext import commands
+
+# from discord_slash import SlashCommand
+# from util.help import CustomHelp
+
+intents = disnake.Intents(messages=True, guilds=True)
 
 # the initial sql query, to add tables
-sql_query = "CREATE TABLE IF NOT EXISTS prefixes(guild BIGINT PRIMARY KEY, prefix VARCHAR(125));" \
-            "CREATE TABLE IF NOT EXISTS lang(guild BIGINT PRIMARY KEY, lang VARCHAR(2));" \
-            "CREATE TABLE IF NOT EXISTS commands_logs(time INT NOT NULL, guild BIGINT, cmd VARCHAR(25), usr BIGINT, type INT);" \
-            "CREATE TABLE IF NOT EXISTS guilds_logs(time INT NOT NULL, guild BIGINT, joined BOOLEAN, guild_count INT);" \
-            "CREATE TABLE IF NOT EXISTS gallery(time INT NOT NULL, embed JSON, title VARCHAR(100), " \
-            "usr BIGINT, id VARCHAR(5), message BIGINT, madeby BIGINT);" \
-            "CREATE TABLE IF NOT EXISTS commands_stats_total(time INT NOT NULL, gallery INT, gigachadify INT, meme INT, chadmeter INT, caption INT)"
+sql_query = (
+    "CREATE TABLE IF NOT EXISTS lang(guild BIGINT PRIMARY KEY, lang VARCHAR(2));"
+    "CREATE TABLE IF NOT EXISTS commands_logs(time INT NOT NULL, guild BIGINT, cmd VARCHAR(25), usr BIGINT, type INT);"
+    "CREATE TABLE IF NOT EXISTS guilds_logs(time INT NOT NULL, guild BIGINT, joined BOOLEAN, guild_count INT);"
+    "CREATE TABLE IF NOT EXISTS commands_stats_total(time INT NOT NULL, gallery INT, gigachadify INT, meme INT, chadmeter INT, caption INT)"
+)
 
 
 # Init sentry
-beta = bool(os.getenv("BETA") == "TRUE")
-if not beta:
-    sentry_sdk.init(
-        os.getenv("SENTRY_ENDPOINT"),
-        traces_sample_rate=1.0
-    )
+if not bool(os.getenv("BETA") == "TRUE"):
+    sentry_sdk.init(os.getenv("SENTRY_ENDPOINT"), traces_sample_rate=1.0)
 
 
 async def run():
-    """ The function to run the bot """
+    """
+    Main function, runs the bot.
+    """
 
     credentials = {
-        "user": os.getenv("DB_USER"), 
+        "user": os.getenv("DB_USER"),
         "password": os.getenv("DB_PASSWORD"),
         "database": os.getenv("DB_DATABASE"),
-        "host": os.getenv("DB_HOST"), 
-        "port": os.getenv("DB_PORT")
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT"),
     }
 
     # make the connection to the db
@@ -52,74 +49,68 @@ async def run():
         lang_data = await conn.fetch("SELECT * FROM lang")
 
     # load the bot class
-    gigachad = GigaChad(db=db, prefix_cache=dict(prefix_data), lang_cache=dict(lang_data))
+    bot = Bot(db=db, lang_cache=dict(lang_data))
 
-    # load the slash commands class
-    slash = SlashCommand(gigachad, sync_commands=True, sync_on_cog_reload=True)
-
-    # load cogs
+    #load cogs
     for filename in os.listdir('./cogs'):
 
         if filename.endswith(".py") and not filename.startswith("_"):
-            gigachad.load_extension(f"cogs.{filename[:-3]}")
-
-    gigachad.load_extension("jishaku")
+            bot.load_extension(f"cogs.{filename[:-3]}")
 
     # and finally, start the bot!
-    await gigachad.start(os.getenv("TOKEN"))
+    await bot.start(os.getenv("TOKEN"))
 
 
-class GigaChad(commands.Bot):
-    def __init__(self, db, prefix_cache: dict, lang_cache: dict):
-        """" the bot class, used everywhere.
-        :param db: is passed for easier use, same for
-        :param prefix_cache: & :param lang_cache:
+class Bot(commands.AutoShardedBot):
+    def __init__(self, db, lang_cache: dict):
+        """ "
+        The Bot object
+
+        Parameters
+        ----------
+        db: asyncpg.pool.Pool
+            The database connection
+        lang_cache: dict
+            The language cache
         """
         super().__init__(
-            command_prefix=get_prefix,
-            help_command=CustomHelp(),
+            command_prefix="gc!",
+            # only usable in dms without the message content intent
             case_insensitive=True,
             strip_after_prefix=True,
-            activity=discord.Activity(
-                type=discord.ActivityType.listening,
-                name="@Giga Chad help"
+            activity=disnake.Activity(
+                type=disnake.ActivityType.listening, name="@Giga Chad help"
             ),
-            intents=intents
+            intents=intents,
         )
 
         self.db = db
-        self.prefix_cache = prefix_cache
         self.lang_cache = lang_cache
         self.invite_link = "https://discord.com/api/oauth2/authorize?client_id=843550872293867570&permissions=379904&scope=bot%20applications.commands"
         self.command_list = {
-            'meme': ["meme"], 
-            'gigachadify': ["gigachadify", "Gigachadify"], 
-            'chadmeter': ["chadmeter", "Chadmeter"],
-            'caption': ["caption"],
-            'gallery': ["gallery"]
+            "meme": ["meme"],
+            "gigachadify": ["gigachadify", "Gigachadify"],
+            "chadmeter": ["chadmeter", "Chadmeter"],
+            "caption": ["caption"],
+            "gallery": ["gallery"],
         }
-        
+
     async def on_ready(self) -> None:
-        print('------')
-        print('Logged in as')
+        print("------")
+        print("Logged in as")
         print(self.user.name)
         print(self.user.id)
-        print(discord.__version__)
-        print('------')
+        print(disnake.__version__)
+        print("------")
 
-    async def reload_cache(self, table: str) -> None:
-        """ Reload prefix or lang cache at runtime
-        :param table: can either be "prefixes" or "lang"
+    async def reload_cache(self) -> None:
+        """
+        Reload prefix or lang cache at runtime
         """
         async with self.db.acquire() as conn:
-            if table == "prefixes":
-                data = await conn.fetch("SELECT * FROM prefixes")
-                self.prefix_cache = dict(data)
-                print("Prefix cache loaded")
-            elif table == "lang":
-                data = await conn.fetch("SELECT * FROM lang")
-                self.lang_cache = dict(data)
-                print("Lang cache loaded")
+            data = await conn.fetch("SELECT * FROM lang")
+            self.lang_cache = dict(data)
+            print("Lang cache loaded")
 
 
 loop = asyncio.get_event_loop()
